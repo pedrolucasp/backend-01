1c4ae06cf3f2e2a5ea237bd80f8421f1d94c7dc1
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2024-12-16 00:14:59 -0300

    all: fix db issues inside the container

--- config/database.php
+++ config/database.php
@@ -1,7 +1,9 @@
 <?php
 
 function getDatabaseConnection() {
-  $pdo = new PDO('sqlite:storage/database.sqlite');
+  $dbFile = __DIR__ . '/../storage/database.sqlite';
+
+  $pdo = new PDO('sqlite:' . $dbFile);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   return $pdo;
